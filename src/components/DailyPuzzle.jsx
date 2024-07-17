d2d4b0f6d6a6f129b3a14dffa0ef8cba9fa4aea0
matthew-clarke10 <matthewkclarke10@gmail.com>
2024-07-17 20:34:15 +1000

    Added click events for hint and solution of daily puzzles

--- src/components/DailyPuzzle.jsx
+++ src/components/DailyPuzzle.jsx
@@ -1,12 +1,34 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
 import { Chessboard } from 'react-chessboard';
-import { getRandomPuzzle, handleMove } from '../utils/chessUtils';
-import { capitalise } from '../utils/other';
-import { FaLightbulb } from "react-icons/fa";
+import { Chess } from 'chess.js';
+import { getRandomPuzzle, handleHintClick, handleSolutionClick, handleMove, } from '../utils/chessUtils';
 
 const DailyPuzzle = ({ difficulty }) => {
+  const [randomPuzzle, setRandomPuzzle] = useState(getRandomPuzzle);
+  const [moveIndex, setMoveIndex] = useState(0);
+  const [fen, setFen] = useState(randomPuzzle.fen);
+  const [chess, setChess] = useState(new Chess(randomPuzzle.fen));
   const [boardWidth, setBoardWidth] = useState(300);
+  const [arrows, setArrows] = useState([]);
+  const [squareStyles, setSquareStyles] = useState({});
+  const [hint, setHint] = useState({});
+  const [showHint, setShowHint] = useState(0);
+  const [puzzleSolved, setPuzzleSolved] = useState(false);
+
+  useEffect(() => {
+    setArrows([]);
+    setSquareStyles({});
+    setShowHint(0);
+
+    if (!puzzleSolved) {
+      setHint({
+        from: randomPuzzle.correctMoves[moveIndex].from,
+        to: randomPuzzle.correctMoves[moveIndex].to,
+        color: "rgb(0, 255, 0)",
+      });
+    }
+  }, [moveIndex, randomPuzzle.correctMoves, puzzleSolved]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -25,11 +47,13 @@ const DailyPuzzle = ({ difficulty }) => {
       <div className="h-full flex flex-col justify-between border-text-light">
         <div className="flex flex-col border-y-2 border-text-light">
           <Chessboard
+            customArrows={arrows}
+            customSquareStyles={squareStyles}
             boardWidth={boardWidth}
-            position={getRandomPuzzle().fen}
-            onPieceDrop={(sourceSquare, targetSquare) => {
-              handleMove(sourceSquare, targetSquare);
-            }}
+            position={fen}
+            onPieceDrop={(sourceSquare, targetSquare) =>
+              handleMove(sourceSquare, targetSquare, chess, randomPuzzle, moveIndex, difficulty, setMoveIndex, setFen, setPuzzleSolved)
+            }
           />
         </div>
         <div>
@@ -38,10 +62,10 @@ const DailyPuzzle = ({ difficulty }) => {
           </div>
           <div className="flex text-2xl text-center">
             <div className="flex-1 bg-yellow-300 hover:bg-yellow-400 border-r-2 border-text-light">
-              <button className="w-full">Hint</button>
+              <button onClick={() => handleHintClick(hint, showHint, puzzleSolved, setArrows, setSquareStyles, setShowHint)} className="w-full">Hint</button>
             </div>
             <div className="flex-1 bg-blue-400 hover:bg-blue-500">
-              <button className="w-full">Solution</button>
+              <button onClick={() => handleSolutionClick()} className="w-full">Solution</button>
             </div>
           </div>
 
